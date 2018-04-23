6dde5ae8c01460e5f29d3ecf996f7272b57d077d
sonmezonur <onursonmez6116@gmail.com>
2018-04-23 03:17:24 +0300

    Setup initial environment

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='strangelove',
+    version='0.0.1',
+    author='Strangelove Authors',
+    license='MIT',
+    description='Collabrative Filtering for Movie Datasets',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha', 'Natural Language :: English',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='Collabrative Filtering, Recommendation Systems',
+    packages=find_packages(exclude=['docs', 'tests', 'dataset']),
+    install_requires=open('requirements.txt').read().splitlines(),
+    url='https://github.com/gokcan/strangelove')
